2c1ac6027cc12f0b8691eb73b0ff6ae9caf11dbf
Yonas <yonas.y@gmail.com>
2026-01-23 16:21:21 -0500

    feat: Add project logo.

--- engines/yonasBSD/models/model-A/features/generator/packages/lib/src/core/public/dsl/prelude.rs
+++ engines/yonasBSD/models/model-A/features/generator/packages/lib/src/core/public/dsl/prelude.rs
@@ -1,7 +1,4 @@
 pub use crate::core::public::dsl::{
-    features_phase::FeaturesPhase,
-    final_phase::FinalPhase,
-    packages_phase::PackagesPhase,
-    projects_phase::ProjectsPhase,
-    node::DslNode,
+    features_phase::FeaturesPhase, final_phase::FinalPhase, node::DslNode,
+    packages_phase::PackagesPhase, projects_phase::ProjectsPhase,
 };
